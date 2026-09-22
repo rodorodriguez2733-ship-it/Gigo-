fa9a2978d34955de087120607b01f11a0c37c9f7
rodorodriguez2733-ship-it <rodorodriguez2733@gmail.com>
2026-09-22 17:49:06 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='gigo-barber-independent-serial-v1';
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match('./index.html')));}});
